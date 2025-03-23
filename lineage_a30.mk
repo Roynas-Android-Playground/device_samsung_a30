a6e0f83867790fdafff1fac429546a6b23e9d2d7
royna2544 <whiteshell2544@naver.com>
2025-03-23 13:08:51 +0900

    a30: Init

---
+++ lineage_a30.mk
@@ -0,0 +1,23 @@
+# Copyright (C) 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a30/device.mk)
+
+BUILD_FINGERPRINT := "samsung/a30cis/a30:11/RP1A.200720.012/A305FNXXU6CUI2:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRIVATE_BUILD_DESC="a30cis-user 11 RP1A.200720.012 A305FNXXU6CUI2 release-keys"
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a30
+PRODUCT_NAME := lineage_a30
+PRODUCT_MODEL := SM-A305F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
