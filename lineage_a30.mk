1241679bd6dc0f2b7631901b52763f6214fb3155
Royna2544 <whiteshell2544@naver.com>
2026-07-27 23:17:15 +0900

    a30: inherit Pixelify common product config

--- lineage_a30.mk
+++ lineage_a30.mk
@@ -7,8 +7,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
-# Inherit LineageOS common device config
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit Pixelify common device config
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/a30/device.mk)
